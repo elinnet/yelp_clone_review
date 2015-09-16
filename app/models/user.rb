28c7c5761d87daabf250d01de904a5b3bbdd0dd1
Elin <elinnet@gmail.com>
2015-09-16 10:19:14 +0100

    unit test for ensuring users only able to place one review passes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,4 @@ class User < ActiveRecord::Base
   has_many :reviewed_restaurants, through: :reviews, source: :restaurant
   has_many :reviews
 
-
-  def already_reviewed?
-    Restaurant.find(params[:id]).any?
-  end
-
 end
