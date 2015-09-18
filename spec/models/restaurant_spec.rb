110861630fa16c9babee0c8c3c5d4966db00c505
Elin <elinnet@gmail.com>
2015-09-18 15:31:33 +0100

    update model specs for review average

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -36,3 +36,12 @@ describe 'reviews' do
     end
   end
 end
+
+describe '#average_rating' do
+  context 'no reviews' do
+    it 'returns "N/A" when there are no reviews' do
+      restaurant = Restaurant.create(name: 'The Ivy')
+      expect(restaurant.average_rating).to eq 'N/A'
+    end
+  end
+end
