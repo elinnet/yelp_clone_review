db92d6942403e0cbc62cf6867fee8ebce8db1478
Elin <elinnet@gmail.com>
2015-09-19 19:01:08 +0100

    tests for star_rating method reporting NA passes.  Add of new test for 5 stars.

--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -5,5 +5,10 @@ describe ReviewsHelper, :type => :helper do
     it 'does nothing for not a number' do
       expect(helper.star_rating('N/A')).to eq 'N/A'
     end
+
+    it 'returns give black stars for five' do
+      expect(helper.star_rating(5)).to eq '★★★★★'
+    end
   end
+
 end
